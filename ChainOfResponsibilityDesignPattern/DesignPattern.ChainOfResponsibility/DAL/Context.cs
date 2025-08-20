40bad2cd92906435709aa91fe7cac436248dd055
bhmberkan <berkanburakturgut@gmail.com>
2025-08-20 22:47:28 +0300

    Chain Of Responsibility Design Pattern

--- ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context.cs
+++ ChainOfResponsibilityDesignPattern/DesignPattern.ChainOfResponsibility/DAL/Context.cs
@@ -2,13 +2,15 @@
 
 namespace DesignPattern.ChainOfResponsibility.DAL
 {
-    public class Context :DbContext
+    public class Context : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-AV1UIG0; initial catalog=DesignPattern1; integrated Security=true;TrustServerCertificate=Yes;");
         }
 
-        public  DbSet<CustomerProcess> customerProcesses { get; set; }
+        public DbSet<CustomerProcess> customerProcesses { get; set; }
+
+        // master olarak oluşturmak  için bir yorum satırı ekliyorum. 
     }
 }
