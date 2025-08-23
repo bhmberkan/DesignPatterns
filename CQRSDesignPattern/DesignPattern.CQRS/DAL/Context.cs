c010d231d8556678ee02d49566da08141692ea54
bhmberkan <berkanburakturgut@gmail.com>
2025-08-23 23:36:22 +0300

    İlgili Proje oluşturuldu. Data access Layer DAL klasörü oluşturuldu. Context ve product sınıfları yazıldı. İlgili paketler geçildi.(sqlserver,core,tools,design)

---
+++ CQRSDesignPattern/DesignPattern.CQRS/DAL/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.CQRS.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-AV1UIG0; initial catalog=DesignPattern2; integrated Security=true;TrustServerCertificate=Yes;");
+        }
+
+        public DbSet<Product> products { get; set; }
+    }
+}
