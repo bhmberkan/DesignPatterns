bcf939c2ec1ab0f9b780d371bb939f1003ba6bb1
bhmberkan <berkanburakturgut@gmail.com>
2025-09-04 00:03:42 +0300

    CompositeDesignPattern

---
+++ CompositeDesignPattern/DesignPatter.Composite/DAL/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPatter.Composite.DAL
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public int UpperCategoryID { get; set; } // bir üstte bulunan kategory ıdsi ne ?
+        public List<Product> products { get; set; }
+    }
+}
---
+++ CompositeDesignPattern/DesignPatter.Composite/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPatter.Composite.DAL
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-AV1UIG0; initial catalog=DesignPattern7; integrated Security=true;TrustServerCertificate=Yes;");
+        }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Category> categories { get; set; }
+    }
+}
