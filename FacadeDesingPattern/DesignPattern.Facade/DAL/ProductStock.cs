477101565d3f14f91ba3ff1619955d26e59182c3
bhmberkan <berkanburakturgut@gmail.com>
2025-09-08 23:37:36 +0300

    FacadeDesingPattern-1

---
+++ FacadeDesingPattern/DesignPattern.Facade/DAL/ProductStock.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPattern.Facade.DAL
+{
+    public class ProductStock
+    {
+        Context context = new Context();
+
+        public void StockDecrease(int id,int amount)
+        {
+            var value = context.products.Find(id);
+            value.ProductStock -= amount;
+            context.SaveChanges();
+        }
+    }
+}
