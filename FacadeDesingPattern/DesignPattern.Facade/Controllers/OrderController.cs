79d2a5483162b0c956fc90a149b2f3c7a895b8c4
bhmberkan <berkanburakturgut@gmail.com>
2025-09-09 22:58:45 +0300

    FacadeDesingPattern-2

---
+++ FacadeDesingPattern/DesignPattern.Facade/Controllers/OrderController.cs
@@ -0,0 +1,52 @@
+﻿using DesignPattern.Facade.DAL;
+using DesignPattern.Facade.FacadePattern;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPattern.Facade.Controllers
+{
+    public class OrderController : Controller
+    {
+        Context context = new Context();
+
+        [HttpGet]
+        public IActionResult OrderDetailStart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OrderDetailStart(int CustomerID, int ProductID, int OrderID, int ProductCount, decimal ProductPrice)
+        {
+            OrderFacade order = new OrderFacade();
+            order.CompleteOrderDetail(CustomerID, ProductID, OrderID, ProductCount, ProductPrice);
+            return RedirectToAction("Index");
+            
+        }
+
+        [HttpGet]
+        public IActionResult OrderStart()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult OrderStart(int customerID)
+        {
+            OrderFacade orderFacade = new OrderFacade();
+            orderFacade.CompleteOrder(customerID);
+            return RedirectToAction("OrderDetailStart");
+        }
+
+        public IActionResult Index()
+        {
+            var values = context.orderDetails.ToList();
+            return View(values);
+        }
+
+        public IActionResult Orders()
+        {
+            var values = context.orders.ToList();
+            return View(values);
+        }
+    }
+}
